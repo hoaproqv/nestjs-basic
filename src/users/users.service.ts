2197a956fa8172559e3d0e2b6ea83caf9b3715b0
hanhoa <hanminhhoa1997@gmail.com>
2024-05-30 22:39:56 +0700

    end course

---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { genSaltSync, hashSync } from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    return hashSync(password, salt);
+  };
+
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = this.getHashPassword(createUserDto.password);
+
+    return await this.userModel.create(createUserDto);
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return 'Not found user';
+
+    return this.userModel.findOne({ _id: id });
+  }
+
+  async update(updateUserDto: UpdateUserDto) {
+    return this.userModel.updateOne(
+      { _id: updateUserDto._id },
+      { ...updateUserDto },
+    );
+  }
+
+  remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return 'Not found user';
+
+    return this.userModel.deleteOne({ _id: id });
+  }
+}
